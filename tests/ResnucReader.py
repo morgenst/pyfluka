08cfbb2f24cf6657e091b1a446651602b3505a28
morgenst <marcus.matthias.morgenstern@cern.ch>
2016-02-24 01:05:13 +0100

    implement PQ and custom units add test cases for ResnucReader add UsrbinReader test class

--- tests/ResnucReader.py
+++ tests/ResnucReader.py
@@ -15,3 +15,51 @@ class TestResnucReader(unittest.TestCase):
                                         PQ.Activity(2.2300E+10, unc= 0.001845*2.2300E+10)]}}
         data = self.reader.load("ResnucInputTest_tab.lis")
         self.assertItemsEqual(data, res)
+
+    def testTabLisReadMultiDet(self):
+        res = {"AlBa-1s": {"Isotope": [PQ.Isotope(75, 32, 0),
+                                       PQ.Isotope(26, 13, 1)],
+                           "Activity": [PQ.Activity(2.0663E+05, unc= 0.99*2.0663E+05),
+                                        PQ.Activity(2.2300E+10, unc= 0.001845*2.2300E+10)]},
+               "": {"Isotope": [PQ.Isotope(75, 32, 0),
+                                PQ.Isotope(26, 13, 1)],
+                    "Activity": [PQ.Activity(2.0663E+05, unc= 0.99*2.0663E+05),
+                                PQ.Activity(2.2300E+10, unc= 0.001845*2.2300E+10)]}}
+        data = self.reader.load("ResnucInputTestMultiDet_tab.lis")
+        self.assertItemsEqual(data, res)
+
+
+    @unittest.skip("")
+    def testMultiFileReadSingleDet(self):
+        data = self.reader.load(["ResnucInputTest_tab.lis", "ResnucInputTest2_tab.lis"])
+        res = {"AlBa-1s": {"Isotope": [PQ.Isotope(75, 32, 0),
+                                       PQ.Isotope(26, 13, 1)],
+                           "Activity": [PQ.Activity(2.0663E+05, unc= 0.99*2.0663E+05),
+                                        PQ.Activity(2.2300E+10, unc= 0.001845*2.2300E+10)]}}
+        self.assertFalse(True, "To be implemented")
+
+    @unittest.skip("")
+    def testMultiFileReadMultiDet(self):
+        data = self.reader.load(["ResnucInputTest_tab.lis", "ResnucInputTest2_tab.lis"])
+        res = {"AlBa-1s": {"Isotope": [PQ.Isotope(75, 32, 0),
+                                       PQ.Isotope(26, 13, 1)],
+                           "Activity": [PQ.Activity(2.0663E+05, unc= 0.99*2.0663E+05),
+                                        PQ.Activity(2.2300E+10, unc= 0.001845*2.2300E+10)]}}
+        self.assertFalse(True, "To be implemented")
+
+    @unittest.skip("")
+    def testMultiFileReadWeighted(self):
+        data = self.reader.load(["ResnucInputTest_tab.lis", "ResnucInputTest2_tab.lis"])
+        res = {"AlBa-1s": {"Isotope": [PQ.Isotope(75, 32, 0),
+                                       PQ.Isotope(26, 13, 1)],
+                           "Activity": [PQ.Activity(2.0663E+05, unc= 0.99*2.0663E+05),
+                                        PQ.Activity(2.2300E+10, unc= 0.001845*2.2300E+10)]}}
+        self.assertFalse(True, "To be implemented")
+
+    def testMultiFileReadMultiDetWeighted(self):
+        data = self.reader.load(["ResnucInputTest_tab.lis", "ResnucInputTest2_tab.lis"])
+        res = {"AlBa-1s": {"Isotope": [PQ.Isotope(75, 32, 0),
+                                       PQ.Isotope(26, 13, 1)],
+                           "Activity": [PQ.Activity(2.0663E+05, unc= 0.99*2.0663E+05),
+                                        PQ.Activity(2.2300E+10, unc= 0.001845*2.2300E+10)]}}
+        self.assertFalse(True, "To be implemented")
---
+++ tests/UsrbinReader.py
@@ -0,0 +1,14 @@
+__author__ = 'marcusmorgenstern'
+__mail__ = ''
+
+import unittest
+from reader.UsrbinReader import UsrbinReader as UR
+
+class TestUsrbinReader(unittest.TestCase):
+    def setUp(self):
+        self.reader = UR("Activity")
+
+    def testRead(self):
+        d = self.reader.load("UsrbinInputTest.ascii")
+        print d['Dose-2y'][1]
+        print len(d['Dose-2y'][0][0])
