d129898f150a2bc9ef403cd4ce140722d643ce42
morgenst <marcus.matthias.morgenstern@cern.ch>
2016-03-04 02:53:18 +0100

    switch to yamlordereddictloader for ordered dict loading

--- tests/ConfigParser.py
+++ tests/ConfigParser.py
@@ -2,19 +2,20 @@ __author__ = 'marcusmorgenstern'
 __mail__ = ''
 
 import unittest
+import utils.PhysicsQuantities as PQ
 from utils.OrderedYAMLExtension import dump
 from base import ConfigParser, IllegalArgumentError
 from collections import OrderedDict
-import utils.PhysicsQuantities as PQ
 from utils import ureg
+
 
 class TestConfigParser(unittest.TestCase):
     def setUp(self):
         self.f = open("test.yaml", "w")
         self.f2 = open("test2.yaml", "w")
-        self.data = {"plugins": {"a": [1, 2, 3]}, "detectors": {"det1":{"mass": "100 kg"}}}
+        self.data = {"plugins": {"a": [1, 2, 3]}, "detectors": {"det1": {"mass": "100 kg"}}}
         self.data2 = {"plugin": {"a": [1, 2, 3, 4]}}
-        self.data3 = {"plugins" : OrderedDict([(1,0), (3,1), (2,4)])}
+        self.data3 = {"plugins": OrderedDict([(1, 0), (3, 1), (2, 4)])}
         dump(self.data, self.f)
         dump(self.data3, self.f2)
 
@@ -29,7 +30,7 @@ class TestConfigParser(unittest.TestCase):
 
     def test_parseConfig(self):
         d = ConfigParser.parse("test.yaml")
-        res = {"plugins": {"a": [1, 2, 3]}, "detectors": {"det1":{"mass": PQ.Mass(100, ureg.kg)}}}
+        res = {"plugins": {"a": [1, 2, 3]}, "detectors": {"det1": {"mass": PQ.Mass(100, ureg.kg)}}}
         self.assertEqual(d, res)
 
     def test_falseParse(self):
@@ -38,7 +39,7 @@ class TestConfigParser(unittest.TestCase):
 
     def test_parseOrder(self):
         od = ConfigParser.parse("test2.yaml")
-        res = [1,2,3]
+        res = [1, 3, 2]
         self.assertEqual(od['plugins'].keys(), res)
 
     def testAttrParsingTable(self):
