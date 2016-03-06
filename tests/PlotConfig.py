9a7520281abd059dd8a27a1e4f0d3e8a024b15dd
morgenst <marcus.matthias.morgenstern@cern.ch>
2016-03-06 21:42:33 +0100

    add unequals test

--- tests/PlotConfig.py
+++ tests/PlotConfig.py
@@ -12,8 +12,12 @@ class testPlotConfig(unittest.TestCase):
         self.assertEqual(pc.type, '1D')
 
     def testEquals(self):
-        x = {"type" : "1D"}
+        x = {"type": "1D"}
         pc1 = PC("foo", x)
         pc2 = PC("bar", x)
         self.assertEqual(pc1, pc2)
 
+    def testUnEquals(self):
+        pc1 = PC("foo", {"type": "1D"})
+        pc2 = PC("foo", {"type": "2D"})
+        self.assertNotEqual(pc1, pc2)
