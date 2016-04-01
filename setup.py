8740c280641885bbb8568bd8ffb5c9486072bc65
marcusmorgenstern <210486m70324>
2016-04-02 00:06:45 +0200

    add setup.py add build cmd for travis

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pyfluka",
+    version="prototype",
+    packages=find_packages(),
+    scripts=["analysistest/run.py"],
+
+    # Project uses reStructuredText, so ensure that the docutils get
+    # installed or upgraded on the target machine
+    install_requires=['numpy>=1.0',
+                      'matplotlib>=1.3.1',
+                      'networkx>=1.11',
+                      'PyYAML>=3.11',
+                      'tabulate>=0.7.5',
+                      'yamlordereddictloader>=0.1.0'],
+
+    package_data={
+
+    },
+
+    # metadata for upload to PyPI
+    author="Marcus Matthias Morgenstern",
+    author_email="marcus.matthias.morgenstern@cern.ch",
+    description="pyfluka package",
+    license="MIT",
+    keywords="FLUKA MC data analysis physics radiation protection",
+    url="https://github.com/morgenst/pyfluka"
+)
