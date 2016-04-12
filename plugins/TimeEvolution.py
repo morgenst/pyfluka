89c30d5da9ea8f5e7b3859d0f206bdebc5430216
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-04-12 17:59:07 +0200

    add docstrings

--- plugins/TimeEvolution.py
+++ plugins/TimeEvolution.py
@@ -17,14 +17,33 @@ class TimeEvolution(BasePlugin):
         self.irr_time = self._parse_config(kwargs.pop('irr_time'))
 
     def invoke(self, data):
+        """
+        Call method. Initiates calculation
+
+        :param dict data: dictionary holding data to be processed
+        :return:
+        """
         for det in data.keys():
             self._apply_coefficient(data[det])
 
     def _apply_coefficient(self, data):
+        """
+        Application of time evolution coefficients on production yield
+
+        :param dict data: scoring region specific dictionaries with isotopes as key
+        :return:
+        """
         for k, val in data.iteritems():
             val["ProductionYield"] *= self._calculate_evolution_coeff(k)
 
     def _calculate_evolution_coeff(self, isotope):
+        """
+        Simple time evolution based on exp. decay and build up model.
+
+        :param Isotope isotope: isotope
+        :return: time evolution factor
+        :rtype: float
+        """
         half_life = _dh._hl[isotope]
         return (1. - exp(-log(2.) * self.irr_time / half_life)) * exp(-log(2.) * self.cool_time / half_life)
 
