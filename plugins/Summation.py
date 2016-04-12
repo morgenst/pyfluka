b81f3ab31bda7a99be9f151eea9d2c643d5aa0c9
morgenst <marcus.matthias.morgenstern@gmail.com>
2016-04-12 18:09:26 +0200

    add docstrings

--- plugins/Summation.py
+++ plugins/Summation.py
@@ -3,7 +3,16 @@ from base import _global_data, IllegalArgumentError
 
 
 class SummationOperator(BasePlugin):
-    def __init__(self, quantity, stored_quantity = None):
+    """
+    Summation operator
+    """
+    def __init__(self, quantity, stored_quantity=None):
+        """
+        Constructor
+
+        :param PhysicsQuantity quantity: quantity to be summed
+        :param PhysicsQuantity stored_quantity: quantity to store result in
+        """
         self.quantity = quantity
         if stored_quantity:
             self.stored_quantity = stored_quantity
@@ -11,6 +20,12 @@ class SummationOperator(BasePlugin):
             self.stored_quantity = "Summed"+quantity
 
     def invoke(self, data):
+        """
+        Performing calculation
+
+        :param dict data: data dictionary
+        :return:
+        """
         for det, values in data.items():
             if not values.values()[0].has_quantity(self.quantity):
                 raise IllegalArgumentError("Request to sum " + self.quantity + " which is not stored.")
